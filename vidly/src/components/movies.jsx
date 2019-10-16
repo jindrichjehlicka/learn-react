1a2bbb665dc8c2ebff0d0e3d95455cbfbb23d9b9
Jay <jindrichjehlicka@gmail.com>
2019-10-16 10:42:28 +0200

    update

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,42 +1,49 @@
-import React, {Component} from "react";
-import {getMovies} from "../services/fakeMovieService";
-import {getGenres} from "../services/fakeGenreService";
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 import Pagination from "./common/pagination";
-import {paginate} from "../utils/paginate"
-import ListGroup from "./common/listGroup"
+import { paginate } from "../utils/paginate";
+import ListGroup from "./common/listGroup";
+import MoviesTable from "./moviesTable";
 
 class Movies extends Component {
     state = {
         movies: [],
-        genres:[],
+        genres: [],
         pageSize: 4,
         currentPage: 1,
 
     };
 
     componentDidMount() {
-        this.setState({movies:getMovies(), genres:getGenres()})
+        const genres = [{ name: 'All Genres' }, ...getGenres()]
+
+        this.setState({ movies: getMovies(), genres })
     }
 
     handleDelete = movie => {
         const movies = this.state.movies.filter(m => m._id !== movie._id);
-        this.setState({movies});
+        this.setState({ movies });
     };
     handlePageChange = page => {
-        this.setState({currentPage: page});
+        this.setState({ currentPage: page });
     };
 
     handleGenreSelect = genre => {
-       this.setState({selectedGenre:genre})
+        this.setState({ selectedGenre: genre, currentPage: 1 })
     };
 
     render() {
-        const {length: count} = this.state.movies;
-        const {pageSize, currentPage, movies: allMovies, selectedGenre} = this.state;
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, movies: allMovies, selectedGenre } = this.state;
 
         if (count === 0) return <p>There are no more movies!</p>;
 
-        const filtered = selectedGenre ? allMovies.filter(m=>m.genre._id === selectedGenre._id) : allMovies;
+        const filtered =
+            selectedGenre && selectedGenre._id ?
+                allMovies.filter(m => m.genre._id === selectedGenre._id) :
+                allMovies;
+
         const movies = paginate(filtered, currentPage, pageSize);
 
         return (
@@ -50,36 +57,10 @@ class Movies extends Component {
                 </div>
                 <div className="col">
                     <p>Showing {filtered.length} movies</p>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th/>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <button
-                                        onClick={() => this.handleDelete(movie)}
-                                        className="btn btn-danger btn-sm"
-                                    >
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-
+                    <MoviesTable
+                        movies={movies}
+                        onDelete={this.handleDelete}
+                    />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
